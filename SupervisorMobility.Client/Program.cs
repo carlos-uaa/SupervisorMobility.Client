34a813c1b7fb3e22fb3b8bd38b2970a1167dc6ae
Alex Cornejo <alex.cornejo.1993@gmail.com>
2022-10-07 10:27:08 -0500

    Modified layout and configured connection to API

--- SupervisorMobility.Client/Program.cs
+++ SupervisorMobility.Client/Program.cs
@@ -7,7 +7,9 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+// Connection to API
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:8080/api/") });
+
 builder.Services.AddMudServices();
 
 await builder.Build().RunAsync();
