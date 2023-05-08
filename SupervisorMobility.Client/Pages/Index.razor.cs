3f910b36eca0acbbbc596a1e8faf04319c94fa94
Pedro Eduardo MuÑoz Gonzalez <pmunoz@gruposinco.com.mx>
2023-05-08 16:38:05 -0600

    Login Page Design
    Created login page design

--- SupervisorMobility.Client/Pages/Index.razor.cs
+++ SupervisorMobility.Client/Pages/Index.razor.cs
@@ -1,4 +1,5 @@
 ﻿using MudBlazor;
+using System.Text.RegularExpressions;
 
 namespace SupervisorMobility.Client.Pages
 {
@@ -9,5 +10,59 @@ namespace SupervisorMobility.Client.Pages
         {
             new BreadcrumbItem("Home", href: "#", disabled: true)
         };
+
+        private List<BreadcrumbItem> _login = new List<BreadcrumbItem>
+        {
+            new BreadcrumbItem("Login", href: "#", disabled: true)
+        };
+
+        public bool logged = false;
+
+        private bool _processing = false;
+        public string username;
+        public string password;
+        public UserLogin _userLogin = new();
+
+        async Task ProcessSomething()
+        {
+            _processing = true;
+            await Task.Delay(2000);
+            _processing = false;
+        }
+        protected async override Task OnInitializedAsync()
+        {
+            
+        
+        }
+        public double? Amount { get; set; }
+        public int? Weight { get; set; }
+        public string Password { get; set; } = "superstrong123";
+
+        bool isShow;
+        InputType PasswordInput = InputType.Password;
+        string PasswordInputIcon = Icons.Material.Filled.VisibilityOff;
+
+        void ButtonTestclick()
+        {
+            if(isShow)
+            {
+                isShow = false;
+                PasswordInputIcon = Icons.Material.Filled.VisibilityOff;
+                PasswordInput = InputType.Password;
+            }
+            else
+            {
+                isShow = true;
+                PasswordInputIcon = Icons.Material.Filled.Visibility;
+                PasswordInput = InputType.Text;
+            }
+        }
+
+
+        private async Task Login()
+        {
+    
+        }
+
     }
 }
