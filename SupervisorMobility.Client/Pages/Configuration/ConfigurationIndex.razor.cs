013829b676f5734536a174775be8af11d2d45c1d
Pedro Muñoz <pmunoz@gruposinco.com.mx>
2023-08-24 16:59:35 -0600

    Line support user, and Supervisor validations

--- SupervisorMobility.Client/Pages/Configuration/ConfigurationIndex.razor.cs
+++ SupervisorMobility.Client/Pages/Configuration/ConfigurationIndex.razor.cs
@@ -20,6 +20,7 @@ namespace SupervisorMobility.Client.Pages.Configuration
                 new BreadcrumbItem(text: Localizer["home"], href: "#"),
                 new BreadcrumbItem(text: Localizer["configuration"], href: "", disabled: true)
             };
+            await GetUserAsync();
             logged = await HasPropertyAsync();
             if (!logged)
             {
