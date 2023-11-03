5b16f1c349f08992edf3b58ab0648f048f0e310b
Pedro Muñoz <pmunoz@gruposinco.com.mx>
2023-11-03 15:51:23 -0600

    feat!: create Pillar entiy, and modified checklist categories and checklist question to use them in job observation.

--- SupervisorMobility.Client/Data/Entities/ChecklistQuestion.cs
+++ SupervisorMobility.Client/Data/Entities/ChecklistQuestion.cs
@@ -3,13 +3,12 @@
     public class ChecklistQuestion
     {
         public int QuestionID { get; set; }
-        public string Code { get; set; } = string.Empty;
-        public string Description { get; set; } = string.Empty;
         public string Prompt { get; set; } = string.Empty;
+        public int PillarId { get; set; }
+        public int Sequence { get; set; }
+        public string NotGood { get; set; }
         public int CategorySequence { get; set; }
-        public int? AnswerSetID { get; set; }
         public bool? IsActive { get; set; } = false;
         public int ChecklistCategoryId { get; set; }
-        public int QuestionTypeId { get; set; }
     }
 }
