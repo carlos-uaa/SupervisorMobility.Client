8a4e54bd1e29fa4dcd40a1cb45db51a6914de982
Vicente <vmedina@gruposinco.com.mx>
2024-05-08 09:28:40 -0600

    feat: added department selection in hci

--- SupervisorMobility.Client/Data/Entities/HCICategory.cs
+++ SupervisorMobility.Client/Data/Entities/HCICategory.cs
@@ -1,12 +1,13 @@
 ﻿
+
 namespace SupervisorMobility.Client.Data.Entities
 {
     public class HCICategory
     {
 
         public int HCICategoryId { get; set; }
-        public string? Name { get; set; }
-        public string? Description { get; set; }
+        public int ChosenCategoryDepartmentId { get; set; }
+        public Department ChosenCategory { get; set; }
         public DateTime? Date { get; set; }
         public bool? IsActive { get; set; } = true;
     }
