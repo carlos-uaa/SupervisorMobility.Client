85eb73757f40e47bcf6587e3d4ae4c2d968a1f8f
Pedro Muñoz <pmunoz@gruposinco.com.mx>
2024-07-22 19:13:01 -0600

    feat: show images in Analysis Details page

---
+++ SupervisorMobility.Client/Pages/SOSHOE/AnalysisPages/AnalysisDetails.razor.cs
@@ -0,0 +1,99 @@
+using MudBlazor;
+using SupervisorMobility.Client.Data.Entities.SOSAnalysis_Process;
+
+namespace SupervisorMobility.Client.Pages.SOSHOE.AnalysisPages
+{
+    public partial class AnalysisDetails
+    {
+        [Parameter]
+        public int? AnalysisId { get; set; }
+
+        SOSAnalysis _sosAnalysis { get; set; } = new();
+        int cycleId = 0;
+        int IndexAnalysis { get; set; } = 0;
+        private List<string> capturedImages = new List<string>();
+
+
+        //Loading
+        private IList<string> _sourceMsgLoading = new List<string>();
+        private IList<Color> _Colors = new List<Color>() { Color.Default, Color.Primary, Color.Secondary, Color.Success, Color.Info, Color.Default, Color.Primary, Color.Secondary, Color.Success, Color.Info };
+        public bool ShowLoading = true;
+
+        protected async override Task OnInitializedAsync()
+        {
+
+            _sourceMsgLoading.Add($"{Localizer1["Loading1"]}");
+            _sourceMsgLoading.Add($"{Localizer1["Loading2"]}");
+            _sourceMsgLoading.Add($"{Localizer1["Loading3"]}");
+            _sourceMsgLoading.Add($"{Localizer1["Loading4"]}");
+            _sourceMsgLoading.Add($"{Localizer1["Loading5"]}");
+            _sourceMsgLoading.Add($"{Localizer1["Loading6"]}");
+            _sourceMsgLoading.Add($"{Localizer1["Loading7"]}");
+            _sourceMsgLoading.Add($"{Localizer1["Loading8"]}");
+            _sourceMsgLoading.Add($"{Localizer1["Loading9"]}");
+            _sourceMsgLoading.Add($"{Localizer1["Loading10"]}");
+            _sourceMsgLoading.Add($"{Localizer1["Loading11"]}");
+
+            _sosAnalysis = await SOSAnalysisServices.GetSOSAnalysis((int)AnalysisId, true, true, true, true, true);
+            Console.WriteLine("test");
+
+            if (_sosAnalysis.Illustrations != null && _sosAnalysis.Illustrations.Any())
+            {
+                Console.WriteLine("aaa");
+
+                foreach (var analysisImage in _sosAnalysis.Illustrations)
+                {
+                    var image = await SOSAnalysisServices.ShowIlustrationSOSAnalysis(analysisImage.FileUploadId);
+                    Console.WriteLine("aaa");
+                    capturedImages.Add(image);
+                }
+            }
+            cycleId = _sosAnalysis.SOSHub.TrainingTime != null ? GetCycleId(_sosAnalysis.SOSHub.TrainingTime) : 0;
+
+            ShowLoading = false;
+            StateHasChanged();
+        }
+
+
+        public static int GetCycleId(string trainingTime)
+        {
+            string cycleIdString = trainingTime.Split(' ').First();
+
+            if (int.TryParse(cycleIdString, out int cycleId))
+            {
+                return cycleId;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public int getAnalysisIndex()
+        {
+            IndexAnalysis++;
+            int index = IndexAnalysis;
+            return index;
+        }
+
+        public static string ReasonFormat(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                return input;
+            }
+
+            if (!input.StartsWith("("))
+            {
+                input = "(" + input;
+            }
+
+            if (!input.EndsWith(")"))
+            {
+                input = input + ")";
+            }
+
+            return input;
+        }
+    }
+}
