f594bd5d962e72a5d0bd34193a4098fe4f324959
Pedro Muñoz <pmunoz@gruposinco.com.mx>
2024-08-01 13:10:15 -0600

    feat: update analysis, edit times and total

--- SupervisorMobility.Client/Pages/SOSHOE/AnalysisPages/AnalysisUpdate.razor.cs
+++ SupervisorMobility.Client/Pages/SOSHOE/AnalysisPages/AnalysisUpdate.razor.cs
@@ -1,5 +1,12 @@
 using MudBlazor;
 using SupervisorMobility.Client.Data.Entities.SOSAnalysis_Process;
+using System.Globalization;
+using System.Text.RegularExpressions;
+using System.Text;
+using Microsoft.JSInterop;
+using static MudBlazor.Icons;
+using SupervisorMobility.Client.Data.Entities;
+using Microsoft.AspNetCore.Components.Web;
 
 namespace SupervisorMobility.Client.Pages.SOSHOE.AnalysisPages
 {
@@ -9,15 +16,34 @@ namespace SupervisorMobility.Client.Pages.SOSHOE.AnalysisPages
         public int? AnalysisId { get; set; }
 
         SOSAnalysis _sosAnalysis { get; set; } = new();
+        List<SOSAnalysisLogbook> mostRecentLogs { get; set; }
+
         int cycleId = 0;
-        int IndexAnalysis { get; set; } = 0;
         private List<string> capturedImages = new List<string>();
 
+
+        //Show evidence
+        private DialogOptions dialogEvidenceOptions = new() { CloseOnEscapeKey = true, MaxWidth = MaxWidth.Medium, FullWidth = true, CloseButton = true };
+
+        private bool visibleEvidence = false;
+
+        private int photoIndex = 0;
+
+
+        //Commentaries and Logbok
+        private DialogOptions dialogCommentariesOptions = new() { CloseOnEscapeKey = true, MaxWidth = MaxWidth.Medium, FullWidth = true, CloseButton = true };
+        private DialogOptions dialogLogbookOptions = new() { CloseOnEscapeKey = true, MaxWidth = MaxWidth.Medium, FullWidth = true, CloseButton = true };
+
+        private bool visibleCommentaries = false;
+        private bool visibleLogbook = false;
 
         //Loading
         private IList<string> _sourceMsgLoading = new List<string>();
         private IList<Color> _Colors = new List<Color>() { Color.Default, Color.Primary, Color.Secondary, Color.Success, Color.Info, Color.Default, Color.Primary, Color.Secondary, Color.Success, Color.Info };
         public bool ShowLoading = true;
+
+
+        private double totalTime;
 
         protected async override Task OnInitializedAsync()
         {
@@ -35,22 +61,41 @@ namespace SupervisorMobility.Client.Pages.SOSHOE.AnalysisPages
             _sourceMsgLoading.Add($"{Localizer1["Loading11"]}");
 
             _sosAnalysis = await SOSAnalysisServices.GetSOSAnalysis((int)AnalysisId, true, true, true, true, true);
+            if (_sosAnalysis.AnalysisLogbooks != null)
+                mostRecentLogs = _sosAnalysis.AnalysisLogbooks.Take(Math.Min(3, _sosAnalysis.AnalysisLogbooks.Count)).ToList();
+            else
+                mostRecentLogs = new List<SOSAnalysisLogbook>();
 
-            Console.WriteLine("test");
+            #region Tests
+            //Use Example logbooks
+            mostRecentLogs = new List<SOSAnalysisLogbook>
+            {
+                new SOSAnalysisLogbook { RevisedItem = "test", Date = new DateTime(2024, 1, 29), SeniorSupervisor = new User { Name = "Jose Abdala" }, Supervisor = new User { Name = "V. Garita" } },
+                new SOSAnalysisLogbook { RevisedItem = "test2", Date = new DateTime(2024, 3, 15), SeniorSupervisor = new User { Name = "Jose Abdala" }, Supervisor = new User { Name = "V. Garita" } },
+                new SOSAnalysisLogbook {RevisedItem = "test3", Date = new DateTime(2024, 8, 10), SeniorSupervisor = new User { Name = "Jose Abdala" }, Supervisor = new User { Name = "V. Garita" }}
+            };
 
+            #endregion
             if (_sosAnalysis.Illustrations != null && _sosAnalysis.Illustrations.Any())
             {
-                Console.WriteLine("aaa");
 
                 foreach (var analysisImage in _sosAnalysis.Illustrations)
                 {
                     var image = await SOSAnalysisServices.ShowIlustrationSOSAnalysis(analysisImage.FileUploadId);
-                    Console.WriteLine("aaa");
                     capturedImages.Add(image);
                 }
             }
             cycleId = _sosAnalysis.SOSHub.TrainingTime != null ? GetCycleId(_sosAnalysis.SOSHub.TrainingTime) : 0;
-
+            totalTime = _sosAnalysis.SOSHub.Sections
+                .Select(sect =>
+                {
+                    double timeValue;
+                    return double.TryParse(sect.Time, out timeValue) ? timeValue : (double?)null;
+                })
+                .Where(timeValue => timeValue.HasValue)
+                .Select(timeValue => timeValue.Value)
+                .DefaultIfEmpty(0)
+                .Sum();
             ShowLoading = false;
             StateHasChanged();
         }
@@ -69,13 +114,16 @@ namespace SupervisorMobility.Client.Pages.SOSHOE.AnalysisPages
                 return 0;
             }
         }
+        //protected override void OnAfterRender(bool firstRender)
+        //{
+        //    IndexAnalysis = 0;
+        //}
 
-        public int getAnalysisIndex()
-        {
-            IndexAnalysis++;
-            int index = IndexAnalysis;
-            return index;
-        }
+        //protected override void OnParametersSet()
+        //{
+        //    IndexAnalysis = 0;
+        //}
+
 
         public static string ReasonFormat(string input)
         {
@@ -96,5 +144,132 @@ namespace SupervisorMobility.Client.Pages.SOSHOE.AnalysisPages
 
             return input;
         }
+
+        private void OpenEvidenceDialog(int index)
+        {
+            photoIndex = index;
+            visibleEvidence = true;
+
+        }
+
+        private void OpenCommentariesDialog()
+        {
+            visibleCommentaries = true;
+
+        }
+
+        private void CloseCommentariesDialog()
+        {
+            visibleCommentaries = false;
+        }
+
+        private void OpenLogbookDialog()
+        {
+            visibleLogbook = true;
+
+        }
+
+        private void CloseLogbookDialog()
+        {
+            visibleLogbook = false;
+        }
+
+
+        //Highlight critical points
+
+        private string GetFormatedAnalisisText(int sectionIndex, int analisisIndex)
+        {
+            string BaseText = Regex.Replace(_sosAnalysis.SOSHub?.Sections[sectionIndex].Analyses[analisisIndex].Text, @"\*", "").ToString();
+
+            return BaseText;
+        }
+
+
+        private MarkupString GenerateHighlightedText(string text, List<string> criticalPoints)
+        {
+            if (string.IsNullOrEmpty(text) || criticalPoints == null || criticalPoints.Count == 0)
+            {
+                return new MarkupString(text);
+            }
+
+            var normalizedText = Normalize(text);
+            var builder = new StringBuilder();
+            var currentIndex = 0;
+
+            foreach (var criticalPoint in criticalPoints)
+            {
+                var normalizedCriticalPoint = Normalize(criticalPoint);
+                var match = Regex.Match(normalizedText, Regex.Escape(normalizedCriticalPoint), RegexOptions.IgnoreCase | RegexOptions.CultureInvariant);
+
+                if (match.Success)
+                {
+                    var startIndex = match.Index;
+                    var endIndex = startIndex + criticalPoint.Length;
+
+                    builder.Append(text.Substring(currentIndex, startIndex - currentIndex));
+
+                    builder.Append($"<mark>{text.Substring(startIndex, endIndex - startIndex)}</mark>");
+
+                    currentIndex = endIndex;
+                }
+            }
+
+            builder.Append(text.Substring(currentIndex));
+
+            return new MarkupString(builder.ToString());
+        }
+
+        private static string Normalize(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                return string.Empty;
+            }
+
+            return input.Normalize(NormalizationForm.FormD).Where(c => CharUnicodeInfo.GetUnicodeCategory(c) != UnicodeCategory.NonSpacingMark).Aggregate(new StringBuilder(), (sb, c) => sb.Append(c)).ToString().ToLowerInvariant();
+        }
+
+        private string ValidateTime(string time)
+        {
+            if (string.IsNullOrEmpty(time))
+            {
+                return null;
+            }
+
+            var regex = new Regex(@"^[0-9]*\.?[0-9]*$");
+            return regex.IsMatch(time) ? null : "Invalid time format. Only numbers and dots are allowed.";
+        }
+
+        private EventCallback<string> CreateValueChangedCallback(Section section)
+        {
+            return EventCallback.Factory.Create<string>(this, e => UpdateTotal(e, section));
+        }
+
+        private void UpdateTotal(string newValue, Section section)
+        {
+            if (section != null && newValue != null)
+            {
+                section.Time = newValue;
+            }
+
+            totalTime = _sosAnalysis.SOSHub.Sections
+                .Select(sect =>
+                {
+                    double timeValue;
+                    return double.TryParse(sect.Time, out timeValue) ? timeValue : (double?)null;
+                })
+                .Where(timeValue => timeValue.HasValue)
+                .Select(timeValue => timeValue.Value)
+                .DefaultIfEmpty(0)
+                .Sum();
+        }
+
+        private async Task UpdateAnalysis()
+        {
+            Console.WriteLine("uddate");
+
+
+        }
+
     }
 }
