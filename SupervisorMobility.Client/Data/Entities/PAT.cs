cfa556a1e09a99a9689151e909502f27b50f488e
Pedro Muñoz <pmunoz@gruposinco.com.mx>
2024-12-06 09:45:57 -0600

    feat: SaveLeader attribute in Pat Entity

--- SupervisorMobility.Client/Data/Entities/PAT.cs
+++ SupervisorMobility.Client/Data/Entities/PAT.cs
@@ -43,6 +43,7 @@ namespace SupervisorMobility.Client.Data.Entities
         public SOSHub? SOSHub { get; set; }
 
         public string? HistoricalAbility { get; set; }
+        public string? SaveLeader { get; set; } = "S";
 
         public bool IsActive { get; set; }
     }
