dac2917feed97c7446b11de7f62f5e6acaca4069
Marco Aguayo <maguayo@gruposinco.com.mx>
2025-05-08 11:07:08 -0600

    Feat: -changes into Combination Details -Changes into combination Update -Create Holiday Entity -Create Holiday configuration menu card -Create Holiday Page -Create Holiday Service -Create Holiday Components

---
+++ SupervisorMobility.Client/Services/CalendarProductiveService/ICalendarProductiveService.cs
@@ -0,0 +1,26 @@
+﻿namespace SupervisorMobility.Client.Services.CalendarProductiveService
+{
+    public interface ICalendarProductiveService
+    {
+
+        List<Holiday> GetHolidaysInService();
+        bool AddHolidaysInService(List<Holiday> toAdd);
+        bool AddHolidayToService(Holiday ToAdd);
+        bool UpdateHolidayInService(Holiday toUpdate);
+
+
+
+        //Task<List<Holiday>> GetHolidays();
+
+        //Task<Holiday> GetHolidayById(int id);
+
+        //Task<Holiday> CreateHoliday(Holiday holiday);
+        //Task<Holiday> CreateMultipleHolidays(List<Holiday> holidays);
+
+        //Task<bool> UpdateHoliday(Holiday holiday);
+        //Task<bool> UpdateMultipleHolidays(List<Holiday> holiday);
+
+        //Task DeleteHoliday(int id);
+
+    }
+}
