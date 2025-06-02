e6099632343b71ef3747dfde853507dba2959880
Marco Aguayo <maguayo@gruposinco.com.mx>
2025-06-02 11:17:26 -0600

    Feat: New Pat dto for users data ranges -Modify code to read and update data ranges -Add text languages for calendar view -Update  UI for pat -modify code for iterate, read and update data ranges

--- SupervisorMobility.Client/Data/Entities/PatSubordinate.cs
+++ SupervisorMobility.Client/Data/Entities/PatSubordinate.cs
@@ -5,6 +5,12 @@
         public int PatSubordinateId { get; set; }
         public int PatId { get; set; }
         public int UserId { get; set; }
+
+        public List<PatSubordinateDates> PatSubordinateDates { get; set; } = new List<PatSubordinateDates>();
+    }
+
+    public class PatSubordinateDates
+    {
         public DateTime StartDate { get; set; }
         public DateTime? EndDate { get; set; }
     }
--- SupervisorMobility.Client/Pages/Inicio/PATPage/PAT_Details.razor.cs
+++ SupervisorMobility.Client/Pages/Inicio/PATPage/PAT_Details.razor.cs
@@ -18,6 +18,7 @@ namespace SupervisorMobility.Client.Pages.Inicio.PATPage
 
         private PAT? _pat { get; set; } = new();
         private List<Distribution> _distributions { get; set; } = new();
+        private List<User> _UserOfMonth { get; set; } = new();
         private List<User> _UserOfArea { get; set; } = new();
         private List<ILULevel> _LevelsILU { get; set; } = new();
         //private ILURegister[,] ILU_Matrix { get; set; } = new ILURegister[0,0];
@@ -216,49 +217,38 @@ namespace SupervisorMobility.Client.Pages.Inicio.PATPage
                 {
                     PatId = _pat.PATid,
                     UserId = user.UserId,
-                    StartDate = DateTime.Now,
-                    EndDate = null
+                    PatSubordinateDates = new List<PatSubordinateDates>
+                    {
+                        new PatSubordinateDates
+                        {
+                            StartDate = DateTime.Now,
+                            EndDate = null
+                        }
+                    }
                 });
             }
 
 
             foreach (var patSubordinate in _pat.PatSubordinates)
             {
-                if (!_UserOfArea.Any(user => user.UserId == patSubordinate.UserId) && patSubordinate.EndDate == null)
+                var endDate = patSubordinate.PatSubordinateDates?.LastOrDefault()?.EndDate;
+
+                if (!_UserOfArea.Any(user => user.UserId == patSubordinate.UserId) && endDate == null)
                 {
-                    patSubordinate.EndDate = DateTime.Now;
+                    patSubordinate.PatSubordinateDates?.Add(new PatSubordinateDates
+                    {
+                        EndDate = DateTime.Now
+                    });
                 }
-                else if (!_UserOfArea.Any(user => user.UserId == patSubordinate.UserId) && patSubordinate.EndDate != null)
+                else if (!_UserOfArea.Any(user => user.UserId == patSubordinate.UserId) && endDate != null)
                 {
                     _UserOfArea.Add(await UsersServices.GetUserAndCollection(patSubordinate.UserId));
                 }
             }
             StateHasChanged();
 
-            try
-            {
-                if (!string.IsNullOrWhiteSpace(_pat.HistoricalAbility))
-                {
-                    var parsedData = JsonSerializer.Deserialize<List<Dictionary<string, Dictionary<string, double>>>>(_pat.HistoricalAbility);
+           
 
-                    if (parsedData == null || parsedData.Count != 12)
-                        throw new InvalidOperationException("El JSON debe contener datos para exactamente 12 meses.");
-
-                    for (int i = 0; i < parsedData.Count; i++)
-                    {
-                        var monthKey = parsedData[i].Keys.First();
-                        var monthData = parsedData[i][monthKey];
-
-                        monthsDistributionPercentage[i] = monthData.ContainsKey("OR_O") && monthData["OR_O"] != 0.0 ? monthData["OR_O"] : null;
-                        monthsUsersPercentage[i] = monthData.ContainsKey("OR_P") && monthData["OR_P"] != 0.0 ? monthData["OR_P"] : null;
-                    }
-                }
-
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Error preparando los datos: {ex.Message}");
-            }
 
             foreach (var op in _distributions)
             {
@@ -286,6 +276,7 @@ namespace SupervisorMobility.Client.Pages.Inicio.PATPage
             {
                 _pat.PatDistributionComments = new List<PatDistributionComment>();
             }
+
             foreach (var op in _distributions)
             {
                 if (AllRegistersOfPat.FindIndex(r => r.DistributionId == op.DistributionId) != -1)
@@ -341,15 +332,85 @@ namespace SupervisorMobility.Client.Pages.Inicio.PATPage
                 }
             }
 
+            SetHistoricalAbility();
+            LoadHistoricalAbility();
+            
             ShowTable = true;
             StateHasChanged();
             await JSRuntime.InvokeVoidAsync("blazorUtils.setDynamicLeft");
 
         }
 
-        private void updateILULevel()
+        public void SetHistoricalAbility()
         {
-            _newIlu.ILULevelId = auxILU_Level;
+            try
+            {
+                var result = new List<Dictionary<string, Dictionary<string, double>>>();
+
+                for (int i = 0; i < monthsNames.Count; i++)
+                {
+
+                    double or_o = CalculateDistributionPercentageMonthAcumulted(i + 1) ?? 0.0;
+                    double or_p = CalculateUserPercentageMonthAcumulted(i + 1) ?? 0.0;
+
+                    // Si es null o 0, intenta calcularlo
+                    if (or_o == null || or_o == 0.0)
+                    {
+                        or_o = monthsDistributionPercentage[i] ?? (CalculateDistributionPercentageMonthAcumulted(i + 1) ?? 0.0);
+                    }
+
+
+                    if (or_p == null || or_p == 0.0)
+                    {
+                        or_p = monthsUsersPercentage[i] ?? (CalculateUserPercentageMonthAcumulted(i + 1) ?? 0.0);
+                    }
+
+                    var monthData = new Dictionary<string, double>
+            {
+                { "OR_O", or_o },
+                { "OR_P", or_p }
+            };
+
+                    result.Add(new Dictionary<string, Dictionary<string, double>>
+            {
+                { monthsNames[i], monthData }
+            });
+                    Console.WriteLine($"Month {i}: {monthsNames[i]}, OR_O: {or_o}, OR_P: {or_p}");
+                }
+
+                _pat.HistoricalAbility = JsonSerializer.Serialize(result, new JsonSerializerOptions { WriteIndented = true });
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error generando el JSON: {ex.Message}");
+            }
+        }
+        void LoadHistoricalAbility()
+        {
+            try
+            {
+                if (!string.IsNullOrWhiteSpace(_pat.HistoricalAbility))
+                {
+                    var parsedData = JsonSerializer.Deserialize<List<Dictionary<string, Dictionary<string, double>>>>(_pat.HistoricalAbility);
+
+                    if (parsedData == null || parsedData.Count != 12)
+                        throw new InvalidOperationException("El JSON debe contener datos para exactamente 12 meses.");
+
+                    for (int i = 0; i < parsedData.Count; i++)
+                    {
+                        var monthKey = parsedData[i].Keys.First();
+                        var monthData = parsedData[i][monthKey];
+
+                        monthsDistributionPercentage[i] = monthData.ContainsKey("OR_O") && monthData["OR_O"] != 0.0 ? monthData["OR_O"] : (CalculateDistributionPercentageMonth(i + 1) ?? null);
+                        monthsUsersPercentage[i] = monthData.ContainsKey("OR_P") && monthData["OR_P"] != 0.0 ? monthData["OR_P"] : (CalculateUserPercentageMonth(i + 1) ?? null);
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error preparando los datos: {ex.Message}");
+            }
         }
 
 
@@ -621,8 +682,13 @@ namespace SupervisorMobility.Client.Pages.Inicio.PATPage
             year = $"{_yearMonth?.ToString("yyyy")}";
             int monthIndex = DateTime.ParseExact(month, "MMMM", System.Globalization.CultureInfo.CurrentCulture.DateTimeFormat).Month;
             int yearIndex = DateTime.ParseExact(year, "yyyy", System.Globalization.CultureInfo.CurrentCulture.DateTimeFormat).Year;
-            FilterUserMonth();
-
+            
+            Console.WriteLine($"_visibleSubordinateIds: {_visibleSubordinateIds.Count()}");
+            Console.WriteLine($"Before Month: {month}, _Users: {_UserOfArea.Count()}, _Mounth: {_UserOfMonth.Count()}");
+            await Task.Run(() => FilterUserMonth());
+            await Task.Run(() => _UserOfMonth = _UserOfArea.Where(u => _visibleSubordinateIds.Contains(u.UserId)).ToList());
+            Console.WriteLine($"_visibleSubordinateIds: {_visibleSubordinateIds.Count()}");
+            Console.WriteLine($"Selected Month: {month}, _Users: {_UserOfArea.Count()}, _Mounth: {_UserOfMonth.Count()}");
             StateHasChanged();
         }
 
@@ -653,10 +719,28 @@ namespace SupervisorMobility.Client.Pages.Inicio.PATPage
         #endregion
         private void FilterUserMonth()
         {
+           _visibleSubordinateIds = _pat.PatSubordinates
+                                               .Where(ps =>
+                                                   ps.PatSubordinateDates != null && ps.PatSubordinateDates.Any(d =>
+                                                       d.StartDate.Month <= _yearMonth.Value.Date.Month &&
+                                                       (d.EndDate == null || d.EndDate.Value.Month >= _yearMonth.Value.Date.Month)
+                                                   )
+                                               )
+                                               .Select(ps => ps.UserId)
+                                               .ToList();
+        }
+
+        private void FilterUserByMonth(int IndexMonth)
+        {
             _visibleSubordinateIds = _pat.PatSubordinates
-                .Where(ps => (_yearMonth.Value.Date >= ps.StartDate.Date || _yearMonth.Value.Date.Month >= ps.StartDate.Date.Month) && (ps.EndDate == null || _yearMonth.Value.Month <= ps.EndDate.Value.Month))
-                .Select(ps => ps.UserId)
-                .ToList();
+                                                .Where(ps =>
+                                                    ps.PatSubordinateDates != null && ps.PatSubordinateDates.Any(d =>
+                                                        d.StartDate.Month <= IndexMonth &&
+                                                        (d.EndDate == null || d.EndDate.Value.Month >= IndexMonth)
+                                                    )
+                                                )
+                                                .Select(ps => ps.UserId)
+                                                .ToList();
         }
 
 
@@ -817,6 +901,126 @@ namespace SupervisorMobility.Client.Pages.Inicio.PATPage
                         // Tomar el registro más reciente del mes correspondiente
                         var record = context
                             .Where(r => r.AcquisitionDate.HasValue && r.AcquisitionDate.Value.Month == MonthIndex)
+                            .OrderByDescending(r => r.AcquisitionDate)
+                            .FirstOrDefault();
+
+                        if (record != null)
+                        {
+                            if (record.ILULevelId != 0 && record.ILULevelId > 5)
+                                sum++;
+                            if (record.ILULevelId != 0 && record.ILULevelId < 5)
+                                hasLowLevel = true;
+                        }
+                    }
+                }
+
+                var meetsKnowledge = sum >= _pat.KnowledgePercentage;
+
+                if (meetsKnowledge)
+                {
+                    countUserO++;
+                }
+                else if (hasLowLevel && !meetsKnowledge || !hasLowLevel && !meetsKnowledge && sum > 0 || !meetsKnowledge)
+                {
+                    countUserX++;
+                }
+            }
+
+            return (countUserO + countUserX) > 0 ? (double)countUserO / (countUserO + countUserX) * 100 : null;
+        }
+
+       
+
+        private double? CalculateDistributionPercentageMonthAcumulted(int MonthIndex)
+        {
+            if (_pat.KnowledgePercentage == null || !_distributions.Any() || !_UserOfArea.Any() || (MonthIndex > DateTime.Today.Month && _pat.AplicationYear == DateTime.Today.Year))
+                return null;
+
+            // Filtrar usuarios activos en el mes
+            FilterUserByMonth(MonthIndex);
+            var activeUserIds = _visibleSubordinateIds ?? new List<int>();
+            var activeUsers = _UserOfArea.Where(u => activeUserIds.Contains(u.UserId)).ToList();
+
+            countDistO = 0;
+            countDistX = 0;
+
+            int index = 0;
+
+            foreach (var usr in activeUsers)
+            {
+                var role = _pat.PatUserRoles?.FirstOrDefault(r => r.UserId == usr.UserId)?.Role;
+                var isSaveLeaderS = _pat.SaveLeader == "S";
+                var isSaveLeaderC = _pat.SaveLeader == "C";
+                var isRoleRelevant = role == null || role == OperatorRole.Lider || role == OperatorRole.CA;
+
+                if (!((role == null && isSaveLeaderS) || (isRoleRelevant && isSaveLeaderC)))
+                {
+                    index++;
+                    continue;
+                }
+
+                int sum = 0;
+                bool hasLowLevel = false;
+
+                foreach (var op in _distributions)
+                {
+                    if (ILU_Matrix.TryGetValue((op.DistributionId, usr.UserId), out var context) && context != null)
+                    {
+                        var record = context
+                            .Where(r => r.AcquisitionDate.HasValue && r.AcquisitionDate.Value.Month <= MonthIndex)
+                            .OrderByDescending(r => r.AcquisitionDate)
+                            .FirstOrDefault();
+
+                        if (record != null)
+                        {
+                            if (record.ILULevelId != 0 && record.ILULevelId > 5)
+                                sum++;
+                            if (record.ILULevelId != 0 && record.ILULevelId < 5)
+                                hasLowLevel = true;
+                        }
+                    }
+                }
+
+                var meetsKnowledge = sum >= _pat.KnowledgePercentage;
+
+                if (meetsKnowledge)
+                {
+                    countDistO++;
+                }
+                else if ((hasLowLevel && !meetsKnowledge) || (!hasLowLevel && !meetsKnowledge && sum > 0) || !meetsKnowledge)
+                {
+                    countDistX++;
+                }
+                index++;
+            }
+
+            return (countDistO + countDistX) > 0 ? (double)countDistO / (countDistO + countDistX) * 100 : null;
+        }
+
+        private double? CalculateUserPercentageMonthAcumulted(int MonthIndex)
+        {
+            if (_pat.KnowledgePercentage == null || !_distributions.Any() || !_UserOfArea.Any() || (MonthIndex > DateTime.Today.Month && _pat.AplicationYear == DateTime.Today.Year))
+                return null;
+
+            // Filtrar usuarios activos en el mes
+            FilterUserByMonth(MonthIndex);
+            var activeUserIds = _visibleSubordinateIds ?? new List<int>();
+            var activeUsers = _UserOfArea.Where(u => activeUserIds.Contains(u.UserId)).ToList();
+
+            countUserO = 0;
+            countUserX = 0;
+
+            foreach (var op in _distributions)
+            {
+                int sum = 0;
+                bool hasLowLevel = false;
+
+                foreach (var usr in activeUsers)
+                {
+                    if (ILU_Matrix.TryGetValue((op.DistributionId, usr.UserId), out var context) && context != null)
+                    {
+                        var record = context
+                            .Where(r => r.AcquisitionDate.HasValue && r.AcquisitionDate.Value.Month <= MonthIndex)
                             .OrderByDescending(r => r.AcquisitionDate)
                             .FirstOrDefault();
 
