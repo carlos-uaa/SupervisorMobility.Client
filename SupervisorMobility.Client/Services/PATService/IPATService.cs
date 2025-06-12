9ef7c22f27179a9c4756d74e54883ad65974ef0f
Vicente <vmedina@gruposinco.com.mx>
2025-06-12 14:46:19 -0600

    feat: added navigation from job update to PAT

--- SupervisorMobility.Client/Services/PATService/IPATService.cs
+++ SupervisorMobility.Client/Services/PATService/IPATService.cs
@@ -4,6 +4,7 @@
     {
         Task<PAT> CreatePat(PAT pat);
         Task<PAT?> getPat(int patid);
+        Task<int?> getPatByJob(int Jobid, int areaid, int plantid);
         Task<List<PAT>> GetAllPATs();
         Task<List<PAT>> GetAllPATSforSSV(int SSVid);
         Task<List<PAT>> GetAllPATSforSV(int SVid);
