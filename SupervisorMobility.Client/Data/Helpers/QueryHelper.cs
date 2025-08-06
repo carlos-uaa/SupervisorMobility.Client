6c5c89c4ad65485b99573eeca20222b9843c8fce
Vicente <vmedina@gruposinco.com.mx>
2025-08-06 13:48:10 -0600

    feat: added new RTC user feat: added both job observation reports and lup reports

---
+++ SupervisorMobility.Client/Data/Helpers/QueryHelper.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+using System.Web;
+
+namespace SupervisorMobility.Client.Data.Helpers
+{
+    public static class QueryHelper
+    {
+        public static string ToQueryString(object obj)
+        {
+            if (obj == null)
+                return string.Empty;
+
+            var properties = obj.GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance);
+
+            var query = string.Join("&", properties
+                .Where(p => p.GetValue(obj) != null)
+                .Select(p =>
+                {
+                    var value = p.GetValue(obj);
+
+                    // Format DateTime
+                    if (value is DateTime dt)
+                    {
+                        value = dt.ToString("yyyy-MM-dd"); // adjust format as needed
+                    }
+
+                    // URL encode
+                    return $"{HttpUtility.UrlEncode(p.Name)}={HttpUtility.UrlEncode(value.ToString())}";
+                }));
+
+            return query;
+        }
+    }
+}
