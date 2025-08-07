ac0dc93368290d2b6196e37491217032224a4094
Marco Aguayo <maguayo@gruposinco.com.mx>
2025-08-07 16:11:14 -0600

    Feat:
    Add SOSSynopticRequirementsService and related features
    
    - Introduced `SOSSynopticRequirementsService` for managing synoptic requirements, including methods for CRUD operations and illustration handling.
    - Registered `ISynopticRequirementsService` in `Program.cs` for dependency injection.
    - Updated `launchSettings.json` for IIS Express configuration.
    - Created `SynopticRequirementsIndex.razor` for displaying and managing synoptic requirements with search, edit, and delete functionalities.
    - Defined `ISynopticRequirementsService` interface and implemented it in `SynopticRequirementsService` class for backend API interactions.

---
+++ SupervisorMobility.Client/Services/SOS_Services/SOSSynopticRequirementsService/ISynopticRequirementsService.cs
@@ -0,0 +1,16 @@
+﻿
+namespace SupervisorMobility.Client.Services.SOS_Services.SOSSynopticRequirementsService
+{
+    public interface ISynopticRequirementsService
+    {
+        Task<SOSSynopticTableofOperatingRequirements> GetSOSSynopticTableofOperatingRequirements(int SOSSynopticTableofOperatingRequirementsId, bool includeLogbooks = false, bool includeSOS = false, bool includeCollections = false);
+        Task<List<SOSSynopticTableofOperatingRequirements>> GetAllSOSSynopticTableofOperatingRequirements(bool includeLogbooks = false, bool includeSOS = false, bool includeCollections = false);
+        Task<SOSSynopticTableofOperatingRequirements> UpdateSOSSynopticTableofOperatingRequirements(SOSSynopticTableofOperatingRequirements SosEntity);
+        Task<bool> DeleteSOSSynopticTableofOperatingRequirements(int SosEntity_id);
+
+        Task<FileUpload> AddIllustrationToSOSSynopticTableofOperatingRequirements(MultipartFormDataContent? contentfiles, int SOS_SOSSynopticTableofOperatingRequirements_id);
+        Task<string> ShowIlustrationSOSSynopticTableofOperatingRequirements(int idfile);
+
+        Task<bool> RemoveIlustrationFromSOSData(int SOS_SOSSynopticTableofOperatingRequirements_id, int ImageFile_id);
+    }
+}
