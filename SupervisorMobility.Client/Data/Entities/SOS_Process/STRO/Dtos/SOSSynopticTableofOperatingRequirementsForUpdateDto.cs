2c8bf6302bcacdac6e7bfc6c8701b964a88b9fec
BRAYAN CAPETILLO <bcapetillo@gruposinco.com.mx>
2025-09-05 02:02:01 -0600

    feat(dto): add SOSSynopticTableofOperatingRequirementsForUpdateDto dto

---
+++ SupervisorMobility.Client/Data/Entities/SOS_Process/STRO/Dtos/SOSSynopticTableofOperatingRequirementsForUpdateDto.cs
@@ -0,0 +1,20 @@
+namespace SupervisorMobility.Client.Data.Entities.SOS_Process.STRO.Dtos
+{
+    public class SOSSynopticTableofOperatingRequirementsForUpdateDto
+    {
+        public int SOSSynopticTableofOperatingRequirementsId { get; set; }
+        public string? InternalControlNumber { get; set; }
+        public string? ProcessName { get; set; }
+
+        public int? CreatorId { get; set; }
+        public int? ReviewerId { get; set; }
+        public int? ApproverId { get; set; }
+
+        public bool? IsActive { get; set; }
+        public int? SOSHubId { get; set; }
+
+        public List<int>? SOSHubIds { get; set; } = new List<int>();
+
+        public List<SOSSynopticTableRequirementOperationDifficulty>? RequirementDifficulties { get; set; } = new List<SOSSynopticTableRequirementOperationDifficulty>();
+    }
+}
