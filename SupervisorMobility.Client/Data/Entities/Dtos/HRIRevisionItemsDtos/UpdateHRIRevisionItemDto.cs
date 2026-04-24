fad4cf5205fc2161c187b3b39045bec5505f867c
gmtzdev <gerardomtzisc@gmail.com>
2026-04-24 13:05:06 -0600

    feat: Add ItemId and HriId properties to UpdateHRIRevisionItemDto for improved data handling

--- SupervisorMobility.Client/Data/Entities/Dtos/HRIRevisionItemsDtos/UpdateHRIRevisionItemDto.cs
+++ SupervisorMobility.Client/Data/Entities/Dtos/HRIRevisionItemsDtos/UpdateHRIRevisionItemDto.cs
@@ -2,10 +2,13 @@
 {
     public class UpdateHRIRevisionItemDto
     {
+        public int ItemId { get; set; }
+        public int HriId { get; set; }
         public int ItemNumber { get; set; }
         public string RevisionPoint { get; set; }
-        public int? RevisionMethodId { get; set; }
+        public int? RevisionMethodId { get; set; } 
         public int? VeredictId { get; set; }
         public int? FrequencyId { get; set; }
+        public bool delete { get; set; } = false;
     }
 }
