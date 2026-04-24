ad84c7d4fddeb65dfeb9e741eb0a7a521996022c
gmtzdev <gerardomtzisc@gmail.com>
2026-04-24 13:04:39 -0600

    feat: Add properties to UpdateHRIDto for enhanced HRI revision management

--- SupervisorMobility.Client/Data/Entities/Dtos/HRIDtos/UpdateHRIDto.cs
+++ SupervisorMobility.Client/Data/Entities/Dtos/HRIDtos/UpdateHRIDto.cs
@@ -1,6 +1,16 @@
-﻿namespace SupervisorMobility.Client.Data.Entites.Dtos.HRIDtos
+﻿using SupervisorMobility.Client.Data.Entites.Dtos.HRIRevisionItemsDtos;
+
+namespace SupervisorMobility.Client.Data.Entites.Dtos.HRIDtos
 {
     public class UpdateHRIDto
     {
+        public int? HRILinesId { get; set; }       
+        public int? HRIItemId { get; set; }
+        public string? ControlNumber { get; set; }
+        public int? HRIDockId { get; set; }
+        public string? Department { get; set; }
+        public int? SupervisorUserId { get; set; }
+        public int? SSVUserId { get; set; }
+        public List<UpdateHRIRevisionItemDto> ItemsRevised { get; set; } = new List<UpdateHRIRevisionItemDto>();
     }
 }
